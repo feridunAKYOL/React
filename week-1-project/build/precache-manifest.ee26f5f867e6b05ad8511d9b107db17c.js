0835494e15351c48561372415f4bee31643c3ffe
feridun Akyol <feridunakyol34@gmail.com>
2020-06-22 10:45:02 +0200

    week-1 fix the mistakes

---
+++ week-1-project/build/precache-manifest.ee26f5f867e6b05ad8511d9b107db17c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976448796f01615a2cff9780fe258e23",
+    "url": "/React/week-1-project/build/index.html"
+  },
+  {
+    "revision": "eab2181de029d235b562",
+    "url": "/React/week-1-project/build/static/js/2.5670d731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/week-1-project/build/static/js/2.5670d731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85365f8c5968a39b349",
+    "url": "/React/week-1-project/build/static/js/main.9e5ddd73.chunk.js"
+  },
+  {
+    "revision": "75313d0aeb2d6c0a9f75",
+    "url": "/React/week-1-project/build/static/js/runtime-main.0e686d82.js"
+  }
+]);
